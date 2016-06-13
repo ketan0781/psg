ae425f63442ed68d751074d13aa2fd032d9de6b2
ketanpatel <kpatel7581@gmail.com>
2016-06-13 10:50:35 -0400

    new file
    .

--- js/script.js
+++ js/script.js
@@ -1,196 +1,184 @@
- $(window).scroll(function () {  
-     var offset = $('.mobile-btn').offset();  
-       var scrollTop = $(window).scrollTop(); // check the visible top of the browser  
-       if (offset.bottom<scrollTop) $('.mobile-btn').addClass('fixed');  
-       else $('.mobile-btn').removeClass('fixed');  
-      });  
+var checkboxes = 'input[type="checkbox"]',
+    calc_prev = 0;
 
 $(document).ready(function() {
-           $('.pageLink').on('click', function() {
-            $('#link-menu').toggleClass('display')
-            })
-// check where the shoppingcart-div is  
-           
-
-// get number of switch available. 
-            $(function() {
-            var totolDrug = $('#family').find('input[type="checkbox"]');
-            var totalD = totolDrug.length;
-            $('.totalNumb').html('(' + totalD + ')');
-            $('.numOfDrug').html(totalD);
-            });
-// end of number of available switch
-
-    //Cart counter hide and show on cheched box
-    $('#wrapper').on('change', 'input[type="checkbox"]', function() {
-            var AllCheck = $('#wrapper').find('input[type="checkbox"]');
-            if ($(AllCheck).is(':checked')) {
-                $('#wrapper .cust-header button>span').show();
-                $('.mobile-btn button>span').show();
-                $('.d-sign').show();
-                $('.d-sign2').show();
-
-                $('.cust-header div:last-child p').css('background', '#F5F5F5');
-            } else {
-                $('#wrapper .cust-header button>span').hide();
-                 $('.mobile-btn button>span').hide()
-                $('.d-sign').hide();
-                $('.d-sign2').hide();
-                $('.cust-header div:last-child p').css('background', '#FFFFFF')
-            }
-
-        })
-        //end of Cart counter hide/show
-
-    //This for Check-All box only 
-    $('.check-All').on('change', 'input[type="checkbox"]', function() {
-        var allCheck = $('#family').find('input[type="checkbox"]');
-        $(allCheck).prop('checked', this.checked);
-        n2 = allCheck.length;
-        if ($(allCheck).is(':checked')) {
-            $('button>span').text(n2);
-            $('.inputDisabled').attr('disabled', false);
-            var count2 = parseInt(n2 * 150);
-            // $('.count').text(count);
-            animateNum2(count2);
-        } else {
-            $('button>span').text(0);
-            $('.count').text(0);
-        }
+    // checkboxes
+    $('input[type="checkbox"]').each(function(){
+        $(this).on('change', function() {
+            toggleCb($(this));
+            animateCb($(this));
+            addBtn();
+            addNum();
+            youSave($(this));
+            maxBtn($('#max-button'));
+        });
     });
-    // End of Check-ALl box only 
-    
-    $('#family').on('change', '.checkbox', function() {
-
-        if($('#family input[type="checkbox"]:not(:checked)').length){
-             $('.check-All').find('input[type="checkbox"]').prop('checked', false);
-        } else {
-            $('.check-All').find('input[type="checkbox"]').prop('checked', true);
-        }
-        var count;    
-        var countChecked = function() {
-            n = $("#family input:checked").length;
-            $("button>span").text(n);
-            var abc = count = n*150;   
-        };
-         if($(this).is(':checked')){
-            countChecked();
-             animateNum(count);
-        }else{
-            countChecked();
-            animateStop(count)
-        } 
+    // max savings button
+    $('#max-button').click(function() {
+        cascadeCb($(this));
+        return false;
     });
-// ==============================================================================
-
-// animate number ========================================= 
-         var animateNum = function(cnt){
-       
-                $('.total-amount').each(function () {
-                    $(this).prop('Counter',$(this).text()).animate({
-                     Counter: cnt
-                 }, {
-                    duration: 150,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-        }
-        // select All ===
-         var animateNum2 = function(cnt){
-           
-                $('.total-amount').each(function () {
-                    $(this).prop('Counter',$(this).text()).animate({
-                     Counter: cnt
-                 }, {
-                    duration: 500,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-        }
-// animate number stop===========================================        
-        var animateStop = function(cnt){
-            
-                $('.total-amount').each(function () {
-                    $(this).prop('Counter',$(this).text()).animate({
-                     Counter: cnt
-                 }, {
-                    duration: 00,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
-                });
-            });
-        }
-
-    //==================animate on checkbook select all ==========================
-    $('#checkboxAll').on('click', function() {
-        if ($(this).is(':checked')) {
-            var totalbox = $('#family').find('.animate-box');
-            $(totalbox).each(function(i) {
-                var cart = $('.inputDisabled');
-                var imgtofly = $(this);
-                if (imgtofly)
-                    if (imgtofly) {
-                        var imgclone = imgtofly.clone()
-                            .offset({
-                                top: imgtofly.offset().top,
-                                left: imgtofly.offset().left
-                            })
-                            .addClass('animateBox')
-                            .appendTo($('body'))
-                            .animate({
-                                'top': cart.offset().top + 44,
-                                'left': cart.offset().left + 0
-
-                            }, 500, 'easeOutCubic');
-                        imgclone.animate({
-                            'width': 0,
-                            'height': 0
-                        }, function() {
-                            $(this).detach()
-                        });
-                    } else {}
-
-            });
-        } else {
-            // alert('unchecked');
-        }
+    // show more button
+    $('.show-more a').click(function() {
+        showMore($(this));
+        return false;
     });
-    //=================animate on checkbox for single check box====================
-    $('.checkbox').change(function() {
-        if ($(this).is(':checked')) {
-            var cart = $('.inputDisabled');
-            var imgtofly = $(this).parent('.check').prev(".animate-box").eq(0);
-            if (imgtofly) {
-                var imgclone = imgtofly.clone().css('width','0px')
-                    .offset({
-                        top: imgtofly.offset().top,
-                        left: imgtofly.offset().left
-                    })
-                    .addClass('animateBox')
-                    .appendTo($('body'))
-
-                    .animate({
-                        'top': cart.offset().top + 44,
-                        'left': cart.offset().left + 0
-                
-
-                    }, 500);
-                imgclone.animate({
-                    'width': 0,
-                    'height': 0
-                }, function() {
-                    $(this).remove()
-                });
-            }
-        } else {}
+    // show more button
+    $('#add-button').click(function() {
+        if (!$(this).hasClass('disabled')) {
+            alert('To the cart page!');
+        }
+        return false;
+    });
+    // init mobile save block
+    $('#add-save-container').css('bottom', function() {
+        var bottom = $(this).outerHeight();
+        return -Math.abs(bottom);
     })
+});
 
-}); //End of doc ready
+// checkbox function
+function toggleCb(cb) {
+    var cbs = cb.closest('ul').find('li input[type="checkbox"]');
+    if (cb.is(':checked')) {
+        cbs.prop('checked', false).parent('li').addClass('text-muted');
+        cb.prop('checked', true).parent('li').removeClass('text-muted');
+    } else {
+        cb.closest('ul').find('li').removeClass('text-muted');
+    }
+}
+
+// animate function
+function animateCb(cb) {
+    // animate
+    if (cb.is(':checked')) {
+        var addPos = $('#add-button-box').offset(),
+            addW = $('#add-button').outerWidth(),
+            addH = $('#add-button').outerHeight(),
+            labelW = cb.next('label').outerWidth(),
+            labelH = cb.next('label').outerHeight(),
+            labelPos = cb.parent('li').offset();
+        // make the div
+        $('<div/>')
+            .addClass('animated')
+            .css({top:labelPos.top, left:labelPos.left, width:labelW, height:labelH})
+            .appendTo('body')
+            .animate({top:addPos.top, left:addPos.left, width:addW, height:addH}, 500, function(){
+                $(this).fadeOut(250, function(){
+                    $(this).remove();
+                });
+            });
+    }
+}
+
+function addBtn() {
+    if ($('input[type="checkbox"]:checked').length > 0) {
+        // enable
+        $("#add-save-container").animate({bottom: 0}, 500);
+        $('#you-save').show();
+        $('#add-button').removeClass('disabled');
+    } else {
+        // disable
+        $("#add-save-container").animate({
+            bottom: -Math.abs($(this).outerHeight())
+        }, 500);
+        $('#you-save').hide();
+        $('#add-button').addClass('disabled');
+    }
+}
+
+function addNum() {
+    $('#dot').show().text($('input[type="checkbox"]:checked').length);
+    if ($('#dot').text() == 0) {
+        $('#dot').hide()
+    };
+}
+
+////////////////////
+// MAX SAVINGS BUTTON
+////////////////////
+function maxBtn(btn){
+    var bestCbs = $('li.best input[type="checkbox"]'),
+        bestChkCbs = $('li.best input[type="checkbox"]:checked');
+    if (bestCbs.length == bestChkCbs.length) {
+        btn.addClass('disabled').text('Max Savings Selected!');
+    } else {
+        btn.removeClass('disabled').text('Select Max Savings');
+    }
+}
+
+function cascadeCb(btn) {
+    if (!btn.hasClass('disabled')) {
+        $('li.best input[type="checkbox"]:not(:checked)').each(function(i){
+            var el = $(this);
+            setTimeout(function() {
+                el.prop('checked', true).change();
+            }, i * 250);
+        });
+    }
+}
+
+////////////////////
+// YOU SAVE
+////////////////////
+function youSave(cb) {
+    // calculate current value for all checkboxes
+    var calc_cur = 0,
+        calc_old_cur = [];
+    calc_old_cur.push(calc_prev); // push global var
+    $('input[type="checkbox"]:checked').each(function(){
+        calc_cur += parseFloat($(this).val());
+    });
+    calc_cur = calc_cur.toFixed(2);
+    calc_old_cur.push(calc_cur); // push new/current value
+    calc_prev = calc_cur; // set global var
+
+    // if selecting play animation, else no animation for you
+    if (cb.is(':checked')) {
+        animateNum(calc_old_cur[0], calc_old_cur[1]);
+    } else {
+        $('#amt').text(formatNum(calc_old_cur[1]));
+    }
+}
+
+function formatNum(num){
+    while (/(\d+)(\d{3})/.test(num.toString())){
+        num = num.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+    }
+    return num;
+}
+
+function animateNum(old, cur) {
+    var decimal_places = 2,
+        decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+    $('input[type="checkbox"]:checked').each(function(){
+        cur += parseFloat($(this).val());
+    });
+    old = parseFloat(old * 100);
+    $('#amt')
+        .prop('number', old)
+        .animateNumber({
+            number: cur * decimal_factor,
+            numberStep: function(now, tween) {
+                var floored_number = Math.floor(now) / decimal_factor,
+                    target = $(tween.elem);
+                if (decimal_places > 0) {
+                    floored_number = floored_number.toFixed(decimal_places);
+                    floored_number = formatNum(floored_number);
+                }
+                target.text(floored_number);
+            }
+        }, 250);
+}
+
+////////////////////
+// SHOW MORE DRUGS
+////////////////////
+
+function showMore(el) {
+    el.closest('.show-more').prev('ul').toggleClass('hidden');
+    el.closest('.show-more').find('a').each(function(){
+        $(this).toggle();
+    });
+    return false;
+}
